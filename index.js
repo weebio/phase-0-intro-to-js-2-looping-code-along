ef3295fd9e6ff10dc4bc5f149288b0be4d6b26a7
Evaristo <bubbali2002@gmail.com>
2021-07-12 20:54:08 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+function writeCards(people, occasion) {
+  let thank = [];
+  for (let i = 0; i < people.length; i++) {
+    thank.push(`Thank you, ${people[i]}, for the wonderful ${occasion} gift!`);
+    debugger;
+  }
+
+  return thank;
+}
+
+function countDown(count) {
+  while (count >= 0) {
+    console.log(count);
+    count--;
+  }
+}
